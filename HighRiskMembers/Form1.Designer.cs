663b33ff8d691736b2f9c3d2bc17472c0464ccee
cyberserg2k2 <43895006+cyberserg2k2@users.noreply.github.com>
2018-10-29 11:41:01 -0600

    Add project files.

---
+++ HighRiskMembers/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace HighRiskMembers
+{
+    partial class HighRiskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HighRiskForm));
+            this.CheckConnectionLabel = new System.Windows.Forms.Label();
+            this.highRiskMembersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.emortelleDataSet = new HighRiskMembers.EmortelleDataSet();
+            this.highRiskMembersTableAdapter = new HighRiskMembers.EmortelleDataSetTableAdapters.HighRiskMembersTableAdapter();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.GrpLogin = new System.Windows.Forms.GroupBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.highRiskMembersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emortelleDataSet)).BeginInit();
+            this.GrpLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CheckConnectionLabel
+            // 
+            this.CheckConnectionLabel.AutoSize = true;
+            this.CheckConnectionLabel.Location = new System.Drawing.Point(18, 10);
+            this.CheckConnectionLabel.Name = "CheckConnectionLabel";
+            this.CheckConnectionLabel.Size = new System.Drawing.Size(0, 13);
+            this.CheckConnectionLabel.TabIndex = 0;
+            this.CheckConnectionLabel.Visible = false;
+            // 
+            // highRiskMembersBindingSource
+            // 
+            this.highRiskMembersBindingSource.DataMember = "HighRiskMembers";
+            this.highRiskMembersBindingSource.DataSource = this.emortelleDataSet;
+            // 
+            // emortelleDataSet
+            // 
+            this.emortelleDataSet.DataSetName = "EmortelleDataSet";
+            this.emortelleDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // highRiskMembersTableAdapter
+            // 
+            this.highRiskMembersTableAdapter.ClearBeforeFill = true;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(179, 63);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(110, 25);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(179, 105);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(106, 25);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(302, 63);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(153, 30);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(302, 102);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(153, 30);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // GrpLogin
+            // 
+            this.GrpLogin.Controls.Add(this.pictureBox1);
+            this.GrpLogin.Controls.Add(this.btnLogin);
+            this.GrpLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GrpLogin.Location = new System.Drawing.Point(38, 24);
+            this.GrpLogin.Name = "GrpLogin";
+            this.GrpLogin.Size = new System.Drawing.Size(451, 181);
+            this.GrpLogin.TabIndex = 6;
+            this.GrpLogin.TabStop = false;
+            this.GrpLogin.Text = "Login";
+            this.GrpLogin.Enter += new System.EventHandler(this.GrpLogin_Enter);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLogin.BackgroundImage")));
+            this.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control;
+            this.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.Control;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(264, 127);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(153, 48);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.btnLogin.MouseHover += new System.EventHandler(this.btnLogin_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 91);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // HighRiskForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 226);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.CheckConnectionLabel);
+            this.Controls.Add(this.GrpLogin);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.highRiskMembersBindingSource, "AccountNum", true));
+            this.Name = "HighRiskForm";
+            this.Text = "High Risk Members";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.highRiskMembersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emortelleDataSet)).EndInit();
+            this.GrpLogin.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CheckConnectionLabel;
+        private EmortelleDataSet emortelleDataSet;
+        private System.Windows.Forms.BindingSource highRiskMembersBindingSource;
+        private EmortelleDataSetTableAdapters.HighRiskMembersTableAdapter highRiskMembersTableAdapter;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.GroupBox GrpLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HighRiskMembers/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HighRiskMembers
+{
+    public partial class HighRiskForm : Form
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=LICUReports;Initial Catalog=Emortelle;Integrated Security=True");
+
+        public HighRiskForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+              
+                con.Open();
+
+                CheckConnectionLabel.Text = "Connection Successful";
+
+                con.Close();
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show("Error:" + ex);
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            SqlCommand cmd = con.CreateCommand();
+
+
+            cmd.CommandType = CommandType.Text;
+
+            cmd.CommandText = "Select * from Users where CAID = '" + txtUserName.Text + "' and HighRiskFormPassword = '" + txtPassword.Text + "'";
+
+            cmd.ExecuteNonQuery();
+
+                                            
+
+            //DataTable dt = new DataTable();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login success");
+
+            }
+            else
+            {
+
+                MessageBox.Show("Invalid Login");
+
+
+            }
+
+
+            //sa.Fill(dt);
+
+
+
+
+            con.Close();
+
+                                                                    
+        }
+
+        private void GrpLogin_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_MouseHover(object sender, EventArgs e)
+        {
+            btnLogin.BackColor = SystemColors.Control;
+        }
+    }
+}
